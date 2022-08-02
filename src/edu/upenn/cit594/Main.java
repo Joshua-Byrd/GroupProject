365d4e4b0e47e4d6e85b83c9de641c82add9253a
Joshua Byrd <joshuabyrd@outlook.com>
2022-08-02 11:53:47 -0400

    created setup method

--- src/edu/upenn/cit594/Main.java
+++ src/edu/upenn/cit594/Main.java
@@ -1,5 +1,9 @@
 package edu.upenn.cit594;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.AccessDeniedException;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,10 +12,34 @@ public class Main {
     public static void main(String[] args) {
 
 
-        String covidDataFile = null;
-        String propDataFile = null;
-        String popDataFile = null;
-        String logFile = null;
+
+
+    }
+
+    /**
+     * Takes in the array of arguments from main, validates each and returns an array of Files
+     * to be passed to other methods.
+     * @param args an array of String command line arguments
+     * @return an array of File objects.
+     */
+    public Database validateArguments(String[] args) throws FileNotFoundException, AccessDeniedException {
+
+
+        /******* Check that arguments are all well-formed *******/
+
+        //incoming args
+        String covidFile = null;
+        String propFile = null;
+        String popFile = null;
+        String logFileArg = null;
+
+        File covidDataFile;
+        File propDataFile;
+        File popDataFile;
+        File logFile;
+
+        ArrayList<File> fileArrayList = new ArrayList<>();
+
 
         Pattern argsPattern = Pattern.compile("^--(?<name>.+?)=(?<value>.+)$");
 
@@ -24,44 +52,86 @@ public class Main {
 
                 switch (argName) {
                     case("covid"):
-                        if(covidDataFile != null) {
+                        if(covidFile != null) {
                             throw new IllegalArgumentException("Covid data file declared twice.");
                         } else {
-                            covidDataFile = argValue;
+                            covidFile = argValue;
                         }
                         break;
                     case("properties"):
-                        if(propDataFile != null) {
+                        if(propFile != null) {
                             throw new IllegalArgumentException("Properties data file declared twice.");
                         } else {
-                            propDataFile = argValue;
+                            propFile = argValue;
                         }
                         break;
                     case("population"):
-                        if(popDataFile != null) {
+                        if(popFile != null) {
                             throw new IllegalArgumentException("Population data file declared twice.");
                         } else {
-                            popDataFile = argValue;
+                            popFile = argValue;
                         }
                         break;
                     case("log"):
-                        if (logFile != null) {
+                        if (logFileArg != null) {
                             throw new IllegalArgumentException("log file declared twice.");
                         } else {
-                            logFile = argValue;
+                            logFileArg = argValue;
                         }
                         break;
                     default:
                         throw new IllegalArgumentException("Argument name must be 'covid', 'properties'," +
                                 " 'population', or 'log'.");
-                    }
+                }
 
-                } else {
+            } else {
                 //throw exception if any argument is not in the correct form
                 throw new IllegalArgumentException("All arguments must be of the form: --name=value");
             }
+        }
+        /******* Create files and validate *******/
+
+        if (covidFile != null) {
+            covidDataFile = new File(covidFile);
+            if  (!covidDataFile.exists()) {
+                throw new FileNotFoundException("Covid file not found.");
+            } else if (!covidDataFile.canRead()) {
+                throw new AccessDeniedException("Covid file cannot be read.");
+            }
+        }
+
+        if (propFile != null) {
+            propDataFile = new File(propFile);
+            if  (!propDataFile.exists()) {
+                throw new FileNotFoundException("Covid file not found.");
+            } else if (!propDataFile.canRead()) {
+                throw new AccessDeniedException("Covid file cannot be read.");
+            }
+        }
+
+        if (popFile != null) {
+            popDataFile = new File(popFile);
+            if  (!popDataFile.exists()) {
+                throw new FileNotFoundException("Covid file not found.");
+            } else if (!popDataFile.canRead()) {
+                throw new AccessDeniedException("Covid file cannot be read.");
+            }
+        }
+
+        if (logFileArg != null) {
+            logFile = new File(logFileArg);
+            if  (!logFile.exists()) {
+                throw new FileNotFoundException("Covid file not found.");
+            } else if (!logFile.canRead()) {
+                throw new AccessDeniedException("Covid file cannot be read.");
+            }
 
         }
+
     }
+
+
+
+
 
 }
